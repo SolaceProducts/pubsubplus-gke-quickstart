38638ae96cff6dcb420513f93679ef5bbf44ba4a
bczoma <balazs.czoma@solace.com>
2018-05-09 15:23:37 -0400

    fixed image naming issue

--- scripts/copy_solace_image_to_gkr.sh
+++ scripts/copy_solace_image_to_gkr.sh
@@ -44,37 +44,40 @@ echo "`date` INFO: solace_url=$solace_url ,Leftovers: $@"
 
 solace_directory=.
 
-echo "`date` INFO: RETRIEVE SOLACE DOCKER IMAGE"
 echo "###############################################################"
 wget -q -O solace-redirect ${solace_url}
 if [[ ${solace_url} == *"em.solace.com"* ]]; then
-    REAL_LINK=`egrep -o "https://[a-zA-Z0-9\.\/\_\?\=%]*" ${solace_directory}/solace-redirect`
-    LOAD_NAME="`echo $REAL_LINK | awk -v FS="(download/|?)" '{print $2}'`"
-    # a redirect link provided by solace
-    wget -O ${solace_directory}/solos.info -nv  https://products.solace.com/download/${LOAD_NAME}_MD5
+  wget -q -O ${solace_directory}/solace-redirect ${solace_url} || echo "There has been an issue with downloading the redirect"
+  REAL_LINK=`egrep -o "https://[a-zA-Z0-9\.\/\_\?\=%]*" ${solace_directory}/solace-redirect`
+  LOAD_NAME="`echo $REAL_LINK | awk -v FS="(download/|?)" '{print $2}'`"
+  # a redirect link provided by solace
+  wget -O ${solace_directory}/solos.info -nv  https://products.solace.com/download/${LOAD_NAME}_MD5
 else
-    REAL_LINK=${solace_url}
-    # an already-existing load (plus its md5 file) hosted somewhere else (e.g. in an s3 bucket)
-    wget -O ${solace_directory}/solos.info -nv  ${solace_url}.md5
+  REAL_LINK=${solace_url}
+  # an already-existing load (plus its md5 file) hosted somewhere else (e.g. in an s3 bucket)
+  wget -O ${solace_directory}/solos.info -nv  ${solace_url}.md5
 fi
 
 IFS=' ' read -ra SOLOS_INFO <<< `cat ${solace_directory}/solos.info`
 MD5_SUM=${SOLOS_INFO[0]}
 SolOS_LOAD=${SOLOS_INFO[1]}
+if [ -z ${MD5_SUM} ]; then
+  echo "`date` ERROR: Missing md5sum for the Solace load" | tee /dev/stderr | tee /dev/stderr
+  exit 1
+fi
 echo "`date` INFO: Reference md5sum is: ${MD5_SUM}"
+
+echo "`date` INFO: Download from URL provided and validate"
 wget -q -O  ${solace_directory}/${SolOS_LOAD} ${REAL_LINK}
-cd ${solace_directory}
 
-
-echo "`date` INFO: check to make sure we have a complete load"
 LOCAL_OS_INFO=`md5sum ${SolOS_LOAD}`
 IFS=' ' read -ra SOLOS_INFO <<< ${LOCAL_OS_INFO}
 LOCAL_MD5_SUM=${SOLOS_INFO[0]}
 if [ ${LOCAL_MD5_SUM} != ${MD5_SUM} ]; then
-    echo "`date` WARN: Possible corrupt SolOS load, md5sum do not match"
-    exit 1
+  echo "`date` ERROR: Possible corrupt Solace load, md5sum do not match" | tee /dev/stderr
+  exit 1
 else
-    echo "`date` INFO: Successfully downloaded ${SolOS_LOAD}"
+  echo "`date` INFO: Successfully downloaded ${SolOS_LOAD}"
 fi
 
 echo "`date` INFO: LOAD DOCKER IMAGE INTO LOCAL REGISTRY"
@@ -82,25 +85,26 @@ echo "########################################################################"
 if [ `docker images "solace-*" -q` ] ; then docker rmi -f `docker images "solace-*" -q`; fi;
 docker load -i ${solace_directory}/${SolOS_LOAD}
 
-local_repo=`docker images | grep solace`
+local_repo=`docker images "solace-*" | grep solace`
 echo "`date` INFO: Current docker images are:"
 echo ${local_repo}
 
+repoName=`echo $local_repo | awk '{print$1}'`
 tag=`echo $local_repo | awk '{print$2}'`
 imageId=`echo $local_repo | awk '{print$3}'`
 
 echo "`date` INFO: PUSH SOLACE VMR INSTANCE INTO GOOGLE CONTAINER REGISTRY"
 echo "##########################################################################################"
 if [ -z ${DEVSHELL_PROJECT_ID} ]; then DEVSHELL_PROJECT_ID=`gcloud projects list | awk 'FNR>1 {print$1}'`; fi
-docker tag ${imageId} gcr.io/${DEVSHELL_PROJECT_ID}/solos-vmr:${tag}
-gcloud docker -- push gcr.io/${DEVSHELL_PROJECT_ID}/solos-vmr:${tag}
+docker tag ${imageId} gcr.io/${DEVSHELL_PROJECT_ID}/${repoName}:${tag}
+gcloud docker -- push gcr.io/${DEVSHELL_PROJECT_ID}/${repoName}:${tag}
 
 echo "`date` INFO: CLEANUP"
 echo "##########################################"
 
-docker rmi gcr.io/${DEVSHELL_PROJECT_ID}/solos-vmr:${tag}
+docker rmi gcr.io/${DEVSHELL_PROJECT_ID}/${repoName}:${tag}
 docker rmi ${imageId}
 
-export SOLACE_IMAGE_URL=gcr.io/${DEVSHELL_PROJECT_ID}/solos-vmr:${tag}
+export SOLACE_IMAGE_URL=gcr.io/${DEVSHELL_PROJECT_ID}/${repoName}:${tag}
 echo "`date` INFO: Record the image reference in the GCR you will need to for next steps"
-echo "SOLACE_IMAGE_URL=gcr.io/${DEVSHELL_PROJECT_ID}/solos-vmr:${tag}"
+echo "SOLACE_IMAGE_URL=${SOLACE_IMAGE_URL}"
